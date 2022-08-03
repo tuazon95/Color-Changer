0aee5567439e6125287e0f68e839b78a3fe5554d
tuazon95 <62610254+tuazon95@users.noreply.github.com>
2022-08-03 10:23:23 +0200

    Add files via upload

---
+++ Color-Changer/script.js
@@ -0,0 +1,17 @@
+const hexCode = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const button = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColor += hexCode[getRandomNumber()];
+  }
+  
+  color.textContent = hexColor;
+  document.body.style.backgroundColor = hexColor;
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * hexCode.length);
+}
